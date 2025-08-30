4e7a309d6afefeee2f7350ec8085f30d0be522e9
Michael Hesemann <michael@mhesemann.de>
2025-08-30 04:32:48 +0200

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -44,8 +44,8 @@ vim.pack.add({
   { src = 'https://github.com/mbbill/undotree' },
   { src = 'https://github.com/nvim-mini/mini.completion' },
   { src = 'https://github.com/nvim-mini/mini.icons' },
-  { src = 'https://github.com/nvim-mini/mini.snippets' },
   { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/mason-org/mason.nvim' },
 })
 
 require('nvim-treesitter.configs').setup({
@@ -61,7 +61,7 @@ require('nvim-tree').setup({
 require('barbar').setup()
 require('mini.completion').setup()
 require('mini.icons').setup()
-require('mini.snippets').setup()
+require('mason').setup()
 MiniIcons.tweak_lsp_kind()
 
 lsp.config('elixirls', {
