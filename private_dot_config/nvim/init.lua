7a6bd6821bd311c51c56f200a474714725123ed5
Michael Hesemann <michael@mhesemann.de>
2025-09-06 11:23:07 +0200

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -35,6 +35,7 @@ g.undotree_WindowLayout = 3
 g.undotree_SetFocusWhenToggle = 1
 g.undotree_SplitWidth = 45
 g.mapleader = ' '
+g.clipboard = 'osc52'
 
 vim.pack.add({
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
@@ -65,33 +66,20 @@ require('mini.icons').setup()
 local miniclue = require('mini.clue')
 miniclue.setup({
   triggers = {
-    -- Leader triggers
     { mode = 'n', keys = '<Leader>' },
     { mode = 'x', keys = '<Leader>' },
-
-    -- Built-in completion
     { mode = 'i', keys = '<C-x>' },
-
-    -- `g` key
     { mode = 'n', keys = 'g' },
     { mode = 'x', keys = 'g' },
-
-    -- Marks
     { mode = 'n', keys = "'" },
     { mode = 'n', keys = '`' },
     { mode = 'x', keys = "'" },
     { mode = 'x', keys = '`' },
-
-    -- Registers
     { mode = 'n', keys = '"' },
     { mode = 'x', keys = '"' },
     { mode = 'i', keys = '<C-r>' },
     { mode = 'c', keys = '<C-r>' },
-
-    -- Window commands
     { mode = 'n', keys = '<C-w>' },
-
-    -- `z` key
     { mode = 'n', keys = 'z' },
     { mode = 'x', keys = 'z' },
   },
